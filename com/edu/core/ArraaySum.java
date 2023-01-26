d8807297ab5618314eabe54dd23f0dcd6ec811a7
sreenikri <51878458+sreenikri@users.noreply.github.com>
2023-01-26 01:57:55 -0500

    Array sum numbers

---
+++ com/edu/core/ArraaySum.java
@@ -0,0 +1,12 @@
+package com.edu.core;
+
+public class ArraaySum {
+    public static void main(String[] args) {
+        int a[]={18,39,75,68,64,5,39,2};
+
+        int sum=0;
+        for(int i:a)
+            sum=sum+i;
+            System.out.println(sum);
+    }
+}
