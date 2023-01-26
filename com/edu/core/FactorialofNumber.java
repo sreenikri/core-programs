0887cf7d5926e4e1811bb06218f9542986be0616
sreenikri <51878458+sreenikri@users.noreply.github.com>
2023-01-26 01:58:20 -0500

    Factorial of number

---
+++ com/edu/core/FactorialofNumber.java
@@ -0,0 +1,12 @@
+package com.edu.core;
+
+public class FactorialofNumber {
+    public static void main(String[] args) {
+        int num=5;
+        int fact=1;
+        for(int i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        System.out.println("  fact of " +num +" is " +fact);
+    }
+}
