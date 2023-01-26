7018f18e5239daf41e1476ec1a595bcf428fa7bd
sreenikri <51878458+sreenikri@users.noreply.github.com>
2023-01-26 00:00:29 -0500

    Swapping of 2 numbers

--- com/edu/core/SwapNumbers.java
+++ com/edu/core/SwapNumbers.java
@@ -2,11 +2,9 @@ package com.edu.core;
 
 public class SwapNumbers {
     public static void main(String[] args) {
-
         int a=10; int b=30;
         System.out.println("before swap and b values are : " +a +"  " +b);
         int temp=0;
-
       //approach -1
         temp=a;
         a=b;
