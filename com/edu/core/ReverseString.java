7763f0bf2d9bdeb0fc79236a39644bc497c50939
sreenikri <51878458+sreenikri@users.noreply.github.com>
2023-01-25 23:56:34 -0500

    ReverseString class added

---
+++ com/edu/core/ReverseString.java
@@ -0,0 +1,24 @@
+package com.edu.core;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String string="Hello";
+        String rev="";
+        for(int i=0; i<string.length();i++){
+        rev= string.charAt(i)+rev.trim();
+        }
+        System.out.println("Origional String " +string);
+        System.out.println("Reverse of String :"+rev);
+
+        System.out.println("Reverse String : " +new StringBuffer(string).reverse());
+        System.out.println("Reverse String : " +new StringBuilder(string).reverse());
+            rev="";
+        for(char ch: string.toCharArray()){
+            rev= ch+rev;
+        }
+        System.out.println("Reverse of String :"+rev);
+
+
+    }
+
+}
