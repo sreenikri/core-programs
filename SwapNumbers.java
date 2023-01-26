89bf973ca2ffea4cb45fc90dec8d6152abe279dc
sreenikri <51878458+sreenikri@users.noreply.github.com>
2023-01-25 23:07:03 -0500

    swapping two numbers  program

---
+++ SwapNumbers.java
@@ -0,0 +1,31 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+        int a=10; int b=30;
+        System.out.println("before swap and b values are : " +a +"  " +b);
+        int temp=0;
+
+      //approach -1
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after swap and b values are : " +a +"  " +b);
+    // approach-2
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("before swap and b values are : " +a +"  " +b);
+
+     //approach-3
+        a=a*b;
+        b=a/b;
+        a=a/b;
+        System.out.println("before swap and b values are : " +a +"  " +b);
+
+        //approach-4
+        b=a+b-(a=b);
+        System.out.println("before swap and b values are : " +a +"  " +b);
+
+    }
+
+}
